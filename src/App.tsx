4f57bcb3a5ef54adc01c52eaafc5b18573311132
AlgoFlowPlatform <algoflowplatform@gmail.com>
2025-05-06 20:40:32 +0300

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import Router from './router'
 import { WalletConnectionProvider } from './context/WalletContext'
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <WalletConnectionProvider>
-      <Router />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/traders" element={<Traders />} />
+          <Route path="/trader/:id" element={<TraderDetails />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </WalletConnectionProvider>
   )
 }
